a4493d4ba5fc49d3afa1ecfd44bafdcf5bfde527
vKrenev <val.krenev.03@mail.ru>
2024-02-10 17:03:56 +0300

    Init

---
+++ task1/code.cpp
@@ -0,0 +1,32 @@
+﻿#define _USE_MATH_DEFINES
+//#define USE_FLOAT
+
+#include <iostream>
+#include <cmath>
+
+#ifdef USE_FLOAT
+using DataType = float;
+#else
+using DataType = double;
+#endif
+
+int main() {
+    const int arraySize = 10000000;
+    DataType* array = new DataType[arraySize];
+
+    // Заполнение массива синусами
+    for (int i = 0; i < arraySize; ++i) {
+        array[i] = std::sin(i * 2 * M_PI / arraySize);
+    }
+
+    // Вычисление суммы
+    DataType sum = 0;
+    for (int i = 0; i < arraySize; ++i) {
+        sum += array[i];
+    }
+
+    std::cout << "Summed: " << sum << std::endl;
+
+    delete[] array;
+    return 0;
+}
